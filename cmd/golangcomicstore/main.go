67a20c0246c477d2a70eb0c8517728b1c0355141
Brayan López <brayanl@lagash.com>
2020-02-07 08:53:07 -0500

    Ajuste de controlador

--- cmd/golangcomicstore/main.go
+++ cmd/golangcomicstore/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	ping "github.com/brayanlopez8/GolangComicsStore/internal/controller/ping"
+	comicscontroller "github.com/brayanlopez8/GolangComicsStore/internal/controller/comics"
+	pingcontroller "github.com/brayanlopez8/GolangComicsStore/internal/controller/ping"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", ping.Ping)
-	r.GET("/comics", comics.listComics)
+	r.GET("/ping", pingcontroller.Ping)
+	r.GET("/comics", comicscontroller.ListComics)
 	r.Run()
 }
